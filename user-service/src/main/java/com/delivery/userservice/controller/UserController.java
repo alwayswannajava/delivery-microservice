4d85d5db0d41f0ea883e0a1466757df4bef77a43
JaverJaver <mykhailo.kornukh@gmail.com>
2026-04-28 00:48:50 +0300

    remove logs

--- user-service/src/main/java/com/delivery/userservice/controller/UserController.java
+++ user-service/src/main/java/com/delivery/userservice/controller/UserController.java
@@ -2,10 +2,7 @@ package com.delivery.userservice.controller;
 
 import com.delivery.userservice.dto.request.CreateUserRequest;
 import com.delivery.userservice.service.UserService;
-import com.delivery.userservice.service.impl.UserServiceImpl;
 import jakarta.validation.Valid;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
-    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
 
     public UserController(UserService userService) {
@@ -25,9 +21,7 @@ public class UserController {
 
     @PostMapping("/new")
     public ResponseEntity<Void> createUser(@RequestBody @Valid CreateUserRequest createUserRequest) {
-        log.info("------------------------POST REQUEST------------------------");
         userService.create(createUserRequest);
-        log.info("------------------------POST REQUEST END------------------------");
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
--- user-service/src/main/java/com/delivery/userservice/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/delivery/userservice/service/impl/UserServiceImpl.java
@@ -4,14 +4,11 @@ import com.delivery.userservice.dto.request.CreateUserRequest;
 import com.delivery.userservice.mapper.UserMapper;
 import com.delivery.userservice.repository.UserRepository;
 import com.delivery.userservice.service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserServiceImpl implements UserService {
-    private static final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
@@ -23,12 +20,6 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public void create(CreateUserRequest createUserRequest) {
-        log.info("Create user incoming :{}", createUserRequest);
-        try {
-            userRepository.save(userMapper.toUser(createUserRequest));
-        } catch (Exception e) {
-            log.error("Can't create user with data: {}", createUserRequest);
-            log.error("Throwing an exception: {}", e.getMessage());
-        }
+        userRepository.save(userMapper.toUser(createUserRequest));
     }
 }
